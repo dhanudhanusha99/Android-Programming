7328d0c4553e1ef4c8c3bafcbe79ebc8a303dac8
dhanudhanusha99 <77845753+dhanudhanusha99@users.noreply.github.com>
2022-02-08 11:26:23 +0530

    Add files via upload

---
+++ 9th pgm/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.facebook_page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DatePicker simpleDatePicker;
+    Button submit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
+
